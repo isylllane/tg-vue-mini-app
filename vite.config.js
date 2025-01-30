9aac4c156fa5a67b78f59bee0f12541dffdd4ee8
isylllane <Lutik301@gmail.com>
2025-01-30 14:23:16 +0300

    init

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,12 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    allowedHosts: [
+      '3ca07eadd7ee87.lhr.life', // Разрешите этот хост
+      'localhost', // Обычно localhost уже разрешен по умолчанию
+    ],
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
